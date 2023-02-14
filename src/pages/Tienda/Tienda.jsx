c6793a97be8960bfd9bc21fdd36f6b2216e483ae
bm-code <i.benimorales@gmail.com>
2023-02-14 09:16:31 +0100

    Version 1 links

--- src/pages/Tienda/Tienda.jsx
+++ src/pages/Tienda/Tienda.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { useState, useEffect } from 'react';
 
 export default function Tienda() {
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [products, setProducts] = useState([]);
-    const state = {
-      storeProducts: []
-    }
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [products, setProducts] = useState([]);
+    // const state = {
+    //   storeProducts: []
+    // }
 
-    useEffect(() => {
-      // getProducts
-      axios({
-        method: 'get',
-        url: 'http://localhost:8888/flores-eva/tienda/wp-json/wc/v3/products',
-        auth: {
-          username: 'ck_ad4fa73a290b9314e04497e44f1e5549ca9bdcd8',
-          password: 'cs_284d3c8e6abf1ab24db3ca70da6b355d28598596'
-        }
-      })
-      .then(response => console.log(response))
-    }, [])
+    // useEffect(() => {
+    //   // getProducts
+    //   axios({
+    //     method: 'get',
+    //     url: 'http://localhost:8888/flores-eva/tienda/wp-json/wc/v3/products',
+    //     auth: {
+    //       username: 'ck_ad4fa73a290b9314e04497e44f1e5549ca9bdcd8',
+    //       password: 'cs_284d3c8e6abf1ab24db3ca70da6b355d28598596'
+    //     }
+    //   })
+    //   .then(response => console.log(response))
+    // }, [])
     
 
   return (
